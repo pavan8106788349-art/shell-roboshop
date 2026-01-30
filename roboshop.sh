184f6bfb41a4696ca2c4ee3f9beac6165ebf462c
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-01-30 19:15:31 +0530

    roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -1,42 +1,41 @@
-  # #!/bin/bash
+#!/bin/bash
 
-# SG_ID="sg-0ea11a7a79d5c791a" # replace with your ID
-# AMI_ID="ami-0220d79f3f480ecf5"
-# ZONE_ID=Z05813963UHKM0FRNKXB
-# DOMAIN_NAME="daws88s.space"
+SG_ID="sg-076ec9ad23dab2b28" # replace with your ID
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z05013202FKF0ZL12WAOP"
+DOMAIN_NAME="daws88s.online"
 
-# for instance in $@
-# do
-#    INSTANCE_ID=$(aws ec2 run-instances \
-#    --image-id $AMI_ID \
-#    --instance-type t3.micro \
-#    --security-group-ids $SG_ID \
-#    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-#    --query 'Instances[0].InstanceID' \
-#    --output text)
+for instance in $@
+do
+    INSTANCE_ID=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
 
-#    if [ $instance == "frontend" ]; then
-#        IP=$(
-#            aws ec2 describe-instances \
-#            --instance-ids $INSTANCE_ID \
-#            --query 'Reservations[].Instances[].PublicIPAddress' \
-#            --output text
-#        )
-         RECORD_NAME="$DOMAIN_NAME" # daws88s.space
-#     else
-#        IP=$(
-#           aws ec2 describe-instances \
-#           --instance-ids $INSTANCE_ID \
-#           --query 'Reservations[].Instances[].PublicIPAddress' \
-#           --output text
-#        )
-          RECORD_NAME="$DOMAIN_NAME" # daws88s.space
-#     fi
+    if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$DOMAIN_NAME" # daws88s.online
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
+    fi
 
-#     echo "IP Adress: $IP"
-# done
+    echo "IP Address: $IP"
 
-   aws route53 change-resource-record-sets \
+    aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
     {
@@ -58,7 +57,7 @@
         ]
     }
     '
-    
-     echo "record updated for $instance"
 
-#done       
+    echo "record updated for $instance"
+
+done
