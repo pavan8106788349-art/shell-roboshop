58548e9ac2dd41701493ede6545576cca567020a
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-02-01 21:39:18 +0530

    Deleted

--- catalogue.sh
+++ catalogue.sh
@@ -72,4 +72,14 @@ VALIDATE $? "Starting and enabling catalogue"
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y
 
-mongosh --host $MONGODB_HOST </app/db/master-data.js
+INDEX=$(mongosh --host $MONGODB_HOST --quiet --eval 'db.getMongo().getDBNames().indexof("Catalogue")')
+
+if [ $INDEX -1e 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js
+    VALIDTAE $? "Loading products"
+else
+   echo -e "Products already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl restart catalogue
+VALIDATE $? "Restarting catalogue"
