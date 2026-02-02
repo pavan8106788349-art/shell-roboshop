d0cdd5299fc624d5726417a8120e64881b8f871b
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-02-02 18:52:47 +0530

    Deleted

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -29,13 +29,13 @@ VALIDATE(){
 cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 VALIDATE $? "Added RabbitMQ repo"
 
-dnf install rabbitmq-server -y
+dnf install rabbitmq-server -y &>>$LOGS_FILE
 VALIDATE $? "Installing  RabbitMQ Server"
 
-systemctl enable rabbitmq-server
+systemctl enable rabbitmq-server &>>$LOGS_FILE
 systemctl start rabbitmq-server
 VALIDATE $? "Enabled and started rabbitmq"
 
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl add_user roboshop roboshop123 &>>$LOGS_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOGS_FILE
 VALIDATE $? "created user and given permissions"
