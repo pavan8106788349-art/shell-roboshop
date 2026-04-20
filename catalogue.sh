f477e00bd0c2e3292977b80458c891d197d80502
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-04-20 21:53:42 +0530

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -8,7 +8,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 SCRIPT_DIR=$PWD
-MONGODB_HOST=mongodb.daws88s.online
+MONGODB_HOST=mongodb.daws88s.space
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
@@ -65,3 +65,8 @@ systemctl daemon-reload
 systemctl enable catalogue  &>>$LOGS_FILE
 systemctl start catalogue
 VALIDATE $? "Starting and enabling catalogue"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
+
+mongosh --host MONGODB_HOST </app/db/master-data.js
